f3716cff95b67a6861144c06f338ba04fda4c5ef
InnocentThief <github@krebser.info>
2020-09-02 07:31:24 +0200

    Added Quest and QuestStep to database

---
+++ Database/dbo/Tables/Quest.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Quest] (
+    [QuestKey]    UNIQUEIDENTIFIER NOT NULL,
+    [AccountKey]  UNIQUEIDENTIFIER NOT NULL,
+    [Name]        NVARCHAR (50)    NOT NULL,
+    [Description] NVARCHAR (4000)  NOT NULL,
+    CONSTRAINT [PK_Quest] PRIMARY KEY CLUSTERED ([QuestKey] ASC),
+    CONSTRAINT [FK_Quest_Account] FOREIGN KEY ([AccountKey]) REFERENCES [dbo].[Account] ([AccountKey])
+);
+
---
+++ Database/dbo/Tables/QuestStep.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[QuestStep] (
+    [QuestStepKey] UNIQUEIDENTIFIER NOT NULL,
+    [QuestKey]     UNIQUEIDENTIFIER NOT NULL,
+    [Name]         NVARCHAR (50)    NOT NULL,
+    [Description]  NVARCHAR (4000)  NOT NULL,
+    CONSTRAINT [PK_QuestStep] PRIMARY KEY CLUSTERED ([QuestStepKey] ASC),
+    CONSTRAINT [FK_QuestStep_Quest] FOREIGN KEY ([QuestKey]) REFERENCES [dbo].[Quest] ([QuestKey])
+);
+
