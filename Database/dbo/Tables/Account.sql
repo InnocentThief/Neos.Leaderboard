7d859b072615f0405f5fd1cc5bf137e2a9483731
InnocentThief <github@krebser.info>
2020-09-02 07:26:51 +0200

    Initial Solution and Database Project

---
+++ Database/dbo/Tables/Account.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Account] (
+    [AccountKey] UNIQUEIDENTIFIER NOT NULL,
+    [Username]   NVARCHAR (100)   NOT NULL,
+    CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED ([AccountKey] ASC)
+);
+
