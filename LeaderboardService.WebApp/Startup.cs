c26acfe0d030ded10de9fdc112462b18efa37096
InnocentThief <github@krebser.info>
2020-09-02 08:51:45 +0200

    Configuration for IIS

--- LeaderboardService.WebApp/Startup.cs
+++ LeaderboardService.WebApp/Startup.cs
@@ -17,6 +17,10 @@ namespace LeaderboardService.WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AutomaticAuthentication = false;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
